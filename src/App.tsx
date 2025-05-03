04a5c4e7c0d0ebc6d2da2509c1ce4e0f2d1e5427
Sarrray <sa_3185@yahoo.co.jp>
2025-05-03 19:05:52 +0900

    Gitに大文字・小文字の変更を反映させる（正式な値に変更）

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { persistor, store } from "./store";
 import Layout from "./components/Layout/Layout";
-import Home from "./components/Homea/Home";
+import Home from "./components/Home/Home";
 import RoomList from "./components/RoomList/RoomList";
 import ChatRoom from "./components/ChatRoom/ChatRoom";
 import SignalRIndex from "./components/SignalR/SignalRIndex";
-import ProgressbarIndex from "./components/Progressa/ProgressbarIndex";
+import ProgressbarIndex from "./components/Progress/ProgressbarIndex";
 
 const App = () => {
   return (
